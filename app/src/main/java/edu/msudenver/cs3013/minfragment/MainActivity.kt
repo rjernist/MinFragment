5b555cdded82e67f2106b3f07c91365f9e8cdc63
rjernist <rsam1@msudenver.edu>
2025-04-09 18:05:45 -0600

    Initial commit

---
+++ app/src/main/java/edu/msudenver/cs3013/minfragment/MainActivity.kt
@@ -0,0 +1,32 @@
+package edu.msudenver.cs3013.minfragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+
+    fun startFragment(view: View) {
+
+        val blueFragment = BlueFragment.newInstance("FIRST")
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, blueFragment)
+            .addToBackStack(null)
+            .commit()
+    }
+}
